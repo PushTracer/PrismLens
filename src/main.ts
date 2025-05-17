b6d72781a56005cc9082f0038c2003be3a4c92d1
pushme <1141846557@qq.com>
2025-05-17 22:22:28 +0800

    refactor: 将字体导入从main.ts移至index.css
    将字体的导入从main.ts文件移至index.css文件，以统一管理样式资源，提升代码的可维护性

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// 通用字体
-import "vfonts/Lato.css";
-// 等宽字体
-import "vfonts/FiraCode.css";
 // 自定义样式
 import "./assets/css/index.css";
 
